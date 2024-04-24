9dfabfb9351a53cf942af1cd3faee36d88d2e3c5
Auguce <wbyu@bu.edu>
2024-04-24 19:46:57 -0400

    creature contract

--- CreatureNFT.sol
+++ CreatureNFT.sol
@@ -14,14 +14,16 @@ contract CreatureNFT is ERC721URIStorage, Ownable {
     }
 
     mapping(uint256 => CreatureAttributes) public creatureAttributes;
+    mapping(uint256 => uint256) public etherBalances;
 
     event CreatureMinted(uint256 tokenId, address owner);
     event AttributesUpdated(uint256 tokenId, uint256 level, uint256 experience, uint256 cryptoBound);
     event CreatureTraded(uint256 tokenId, address from, address to);
     event LevelUp(uint256 tokenId, uint256 newLevel);
+    event EtherDeposited(uint256 tokenId, uint256 amount, address depositor);
+    event EtherWithdrawn(uint256 tokenId, uint256 amount, address recipient);
 
-    // Pass the initial owner address to the Ownable constructor
-    constructor(address initialOwner) ERC721("CreatureNFT", "CNFT") Ownable(initialOwner) {}
+    constructor(string memory name, string memory symbol, address initialOwner) ERC721(name, symbol) Ownable(initialOwner) {}
 
     function mintCreature(address player, string memory tokenURI) public onlyOwner {
         uint256 newTokenId = _currentTokenId + 1;
@@ -33,36 +35,33 @@ contract CreatureNFT is ERC721URIStorage, Ownable {
         emit CreatureMinted(newTokenId, player);
     }
 
-    function updateCreatureAttributes(uint256 tokenId, uint256 experienceGained, uint256 cryptoToBind) public {
-        require(ownerOf(tokenId) == msg.sender, "Caller is not the owner of the NFT");
-        CreatureAttributes storage attributes = creatureAttributes[tokenId];
-        attributes.experience += experienceGained;
-        attributes.cryptoBound += cryptoToBind;
-
-        // Check if the creature should level up
-        if (attributes.experience >= getRequiredExperience(attributes.level)) {
-            attributes.level++;
-            attributes.experience = 0; // Optionally reset experience after leveling up
-            emit LevelUp(tokenId, attributes.level);
-        }
-
-        emit AttributesUpdated(tokenId, attributes.level, attributes.experience, attributes.cryptoBound);
+    function depositEther(uint256 tokenId) public payable {
+        require(ownerOf(tokenId) != address(0), "Creature does not exist.");
+        require(ownerOf(tokenId) == msg.sender, "Caller is not the owner.");
+        etherBalances[tokenId] += msg.value;
+        emit EtherDeposited(tokenId, msg.value, msg.sender);
     }
 
-        function convertCryptoToExperience(uint256 cryptoAmount) public pure returns (uint256) {
-        //conversion rate: 1 crypto = 10 experience points
-        return cryptoAmount * 10;
+    function withdrawEther(uint256 tokenId, uint256 amount) public {
+        require(ownerOf(tokenId) != address(0), "Creature does not exist.");
+        require(ownerOf(tokenId) == msg.sender, "Caller is not the owner.");
+        require(etherBalances[tokenId] >= amount, "Insufficient balance.");
+
+        etherBalances[tokenId] -= amount;
+        payable(msg.sender).transfer(amount);
+        emit EtherWithdrawn(tokenId, amount, msg.sender);
     }
 
-    function getRequiredExperience(uint256 level) public pure returns (uint256) {
-        // Define how experience requirements scale with levels
-        return (level + 1) * 100; // Reasonable scaling
-    }
-
-    // Override the standard ERC721 transfer function to add custom logic
     function safeTransferCreature(address from, address to, uint256 tokenId) public {
-        emit CreatureTraded(tokenId, from, to);
+        require(ownerOf(tokenId) != address(0), "Creature does not exist.");
+        require(ownerOf(tokenId) == msg.sender, "Caller is not the owner.");
+        
         safeTransferFrom(from, to, tokenId);
+        uint256 balance = etherBalances[tokenId];
+        etherBalances[tokenId] = 0;
+        if (balance > 0) {
+            payable(to).transfer(balance);
+        }
+        emit CreatureTraded(tokenId, from, to);
     }
 }
-
